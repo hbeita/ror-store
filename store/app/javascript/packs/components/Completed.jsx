a2a16257a0c1882d7ff5210ebac46835830e84db
hbeita <hecbeita1988@gmail.com>
2021-01-12 20:38:16 -0600

    Add todos components.

---
+++ store/app/javascript/packs/components/Completed.jsx
@@ -0,0 +1,5 @@
+// components/Home.jsx
+import React, { useState, useEffect } from 'react';
+import Loader from './Loader';
+import Pending from './Pending';
+import Completed from './Completed';
--- store/db/schema.rb
+++ store/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_163522) do
+ActiveRecord::Schema.define(version: 2021_01_12_211741) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2021_01_12_163522) do
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.string "title"
+    t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
